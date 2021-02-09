ae8d00c186a3f3ab9b271931df782c5028fd9171
Simonanilgeorge <smnanilgeorge@gmail.com>
2021-02-09 15:07:35 +0530

    added dates to the filter array

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import {UploadService} from '../../providers/upload.service'
+import { UploadService } from '../../providers/upload.service'
 import * as _ from 'lodash';
 
 
@@ -12,51 +12,76 @@ import * as _ from 'lodash';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('checkboxes') private myCheckbox: any;
+
   @ViewChild('UploadFileInput', { static: false }) uploadFileInput: ElementRef;
   fileUploadForm: FormGroup;
   fileInputLabel: string;
+  SLAExpirationFilter: String[] = []
+  titles: String[] = [];
+  dates: String[] = [];
+  datas: any = null;
+  div1: boolean = true;
+  div2: boolean = false;
+  flag: Number = 0;
+  columnTotal = 0;
+  columnSum = {};
+  expanded: Boolean = false;
 
-  titles:String[]=[];
-  dates:String[]=[];
-  datas:any=null;
-  div1:boolean=true;
-  div2:boolean=false;
-  flag:Number=0;
-  columnTotal=0;
-  columnSum={};
-  expanded:Boolean = false;
-
-  constructor(private formBuilder: FormBuilder,private uploadService:UploadService) { }
+  constructor(private formBuilder: FormBuilder, private uploadService: UploadService) { }
 
   ngOnInit(): void {
     this.fileUploadForm = this.formBuilder.group({
       rvsi: [''],
-      sp2:['']
+      sp2: ['']
 
     });
-    
+
   }
 
-  divFunction(){
-    this.div1=!this.div1;
-    this.div2=!this.div2;
+  divFunction() {
+    this.div1 = !this.div1;
+    this.div2 = !this.div2;
     this.ngOnInit();
-    
-}
 
- showCheckboxes() {
-  // var checkboxes = document.getElementById("checkboxes");
-
-  console.log(this.myCheckbox)
-  if (!this.expanded) {
-
-    this.expanded = true;
-  } else {
-
-    this.expanded = false;
   }
-}
+
+  showCheckboxes() {
+    // var checkboxes = document.getElementById("checkboxes");
+
+
+    if (!this.expanded) {
+
+      this.expanded = true;
+    } else {
+
+      this.expanded = false;
+    }
+  }
+
+//Add the dates to the array 
+
+  filter(date) {
+
+    console.log(date);
+    // check if date already exists in the array
+    if (!this.SLAExpirationFilter.includes(date)) {
+
+      this.SLAExpirationFilter.push(date);
+    }
+    else {
+
+      // this block removes the date from the array
+      let index;
+      index = this.SLAExpirationFilter.findIndex((sla) => {
+        return sla == date;
+      })
+
+      console.log(index)
+      this.SLAExpirationFilter.splice(index, 1)
+    }
+    console.log(this.SLAExpirationFilter)
+
+  }
   onFileSelect(event) {
     this.titles = [];
     let af = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']
@@ -66,89 +91,89 @@ export class HomeComponent implements OnInit {
       // console.log(file);
 
       if (!_.includes(af, file1.type || file2.type)) {
-        alert('Only EXCEL Docs Allowed!');  
-      } 
-      else{
+        alert('Only EXCEL Docs Allowed!');
+      }
+      else {
         this.fileUploadForm.get('rvsi').setValue(file1);
         this.fileUploadForm.get('sp2').setValue(file2);
-        this.flag=0;
+        this.flag = 0;
         //console.log(this.fileUploadForm.value);
       }
-      }
-    else{
-  
-      this.flag=1;
-      
     }
+    else {
+
+      this.flag = 1;
+
     }
+  }
 
   onFormSubmit() {
 
-    if(this.flag==1){
+    if (this.flag == 1) {
       alert("you can only choose two files")
       return;
 
     }
-    else{
-    if (!this.fileUploadForm.get('rvsi').value && !this.fileUploadForm.get('sp2').value) {
-      alert('Please fill valid details!');
-      return false;
-    }
-
-    const formData = new FormData();
-    formData.append('rvsi', this.fileUploadForm.get('rvsi').value);
-    formData.append('sp2',this.fileUploadForm.get('sp2').value)
-
-    this.uploadService.uploadFile(formData).subscribe((res)=>{  
-
-      this.datas=JSON.parse(res);
-      
-      if((Object.keys(this.datas)).includes("error_in_file")){
-        alert("Please choose a proper file with vaild details");
+    else {
+      if (!this.fileUploadForm.get('rvsi').value && !this.fileUploadForm.get('sp2').value) {
+        alert('Please fill valid details!');
+        return false;
       }
-      else{
-        
-        this.datas.forEach((data)=>{
-          for(let d in data){
-            if(d == "SLAExpiration"){ 
-              this.dates = data[d]
-            }
-            if(this.titles.includes(d) || d=="Task_Name" || d=="null" || d=="Grand_total" || d=="SLAExpiration"){
-              continue;
-            }
-            else{
-              this.columnSum[d] = 0;
-              this.titles.push(d);
-            }
-          }
-        })
-        this.datas.pop()
 
-  
-        console.log(this.datas)
-        this.columnSum["Grand_total"] = 0;
-        this.datas.forEach((data)=>{
-          for(let row in data){
-              if(row != "Task_Name" && row != "null"){
-                this.columnSum[row]=this.columnSum[row]+data[row];
-              }
-              
-          }
-        })
-        
-        this.titles=this.titles.sort();
-        this.divFunction();
-        if (res.statusCode === 200) {
-          // Reset the file input
-          this.uploadFileInput.nativeElement.value = "";
-          this.fileInputLabel = undefined;
+      const formData = new FormData();
+      formData.append('rvsi', this.fileUploadForm.get('rvsi').value);
+      formData.append('sp2', this.fileUploadForm.get('sp2').value)
+
+      this.uploadService.uploadFile(formData).subscribe((res) => {
+
+        this.datas = JSON.parse(res);
+
+        if ((Object.keys(this.datas)).includes("error_in_file")) {
+          alert("Please choose a proper file with vaild details");
         }
-      }
-    },(err)=>{
-      console.log(err.message);
-    })
+        else {
+
+          this.datas.forEach((data) => {
+            for (let d in data) {
+              if (d == "SLAExpiration") {
+                this.dates = data[d]
+              }
+              if (this.titles.includes(d) || d == "Task_Name" || d == "null" || d == "Grand_total" || d == "SLAExpiration") {
+                continue;
+              }
+              else {
+                this.columnSum[d] = 0;
+                this.titles.push(d);
+              }
+            }
+          })
+          this.datas.pop()
+
+
+          console.log(this.datas)
+          this.columnSum["Grand_total"] = 0;
+          this.datas.forEach((data) => {
+            for (let row in data) {
+              if (row != "Task_Name" && row != "null") {
+                this.columnSum[row] = this.columnSum[row] + data[row];
+              }
+
+            }
+          })
+
+          this.titles = this.titles.sort();
+          this.divFunction();
+          if (res.statusCode === 200) {
+            // Reset the file input
+            this.uploadFileInput.nativeElement.value = "";
+            this.fileInputLabel = undefined;
+          }
+        }
+      }, (err) => {
+        console.log(err.message);
+      })
+    }
   }
-}
 }
 
 
