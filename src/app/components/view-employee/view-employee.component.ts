0aef89fd19775b353d596677218592df3e94e47b
Simon Anil George <Simon@Anils-MBP.domain.name>
2021-05-28 16:56:39 +0530

    modal box for view employee

--- src/app/components/view-employee/view-employee.component.ts
+++ src/app/components/view-employee/view-employee.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 import { Router, ActivatedRoute } from '@angular/router';
 import { EmpreportService } from '../../providers/empreport.service';
 import { LoginService } from '../../providers/login.service'
+
 
 @Component({
   selector: 'app-view-employee',
@@ -11,14 +12,15 @@ import { LoginService } from '../../providers/login.service'
 })
 export class ViewEmployeeComponent implements OnInit {
 
-
+  modalBoolean: Boolean = false
   titles = [];
+dataToBeDeleted;
   titleName;
-  message =null;
+  message = null;
   toast: Boolean = false;
   searchedKeyword;
   data = [];
-  flag:Boolean=false;
+  flag: Boolean = false;
   constructor(private empReportService: EmpreportService, private router: Router, private loginService: LoginService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -41,7 +43,7 @@ export class ViewEmployeeComponent implements OnInit {
         return Object.keys(data);
       })[0];
       this.titles.pop()
-      this.flag=true
+      this.flag = true
     }, (err) => {
       console.log(err.message);
     })
@@ -57,12 +59,13 @@ export class ViewEmployeeComponent implements OnInit {
   }
   delete(data) {
 
-
-    this.empReportService.deleteEmployee(data).subscribe((res)=>
-    {
+this.modalBoolean=false;
+    this.empReportService.deleteEmployee(data).subscribe((res) => {
+      
       this.showToastMessage("Deleted successfully")
+      
       this.ngOnInit();
-    },(err)=>{
+    }, (err) => {
       this.showToastMessage("Deletion failed")
       console.log(err.message)
     })
@@ -76,14 +79,28 @@ export class ViewEmployeeComponent implements OnInit {
     }, 2000)
   }
 
-  getTitleName(title){
+  getTitleName(title) {
 
-    this.titleName=null;
-    setTimeout(()=>{
-      this.titleName=title;
-    },100)
+    this.titleName = null;
+    setTimeout(() => {
+      this.titleName = title;
+    }, 100)
 
 
+  }
+  showModal(data) {
+    // (click)="delete(data)" 
+    console.log(data)
+
+
+    this.modalBoolean = true;
+    this.dataToBeDeleted=data;
+  }
+
+  closeModal() {
+
+    this.modalBoolean = false;
+    this.dataToBeDeleted=null;
   }
 
 }
