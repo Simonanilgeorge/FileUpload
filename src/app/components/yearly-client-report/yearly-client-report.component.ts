c385874bd4228294578d658d9d1cbc6d2d3f5fd4
Simon Anil George <Simon@Anils-MBP.domain.name>
2021-07-02 16:11:33 +0530

    yearly client report flag

--- src/app/components/yearly-client-report/yearly-client-report.component.ts
+++ src/app/components/yearly-client-report/yearly-client-report.component.ts
@@ -10,61 +10,75 @@ import { DatePipe } from '@angular/common';
   selector: 'app-yearly-client-report',
   templateUrl: './yearly-client-report.component.html',
   styleUrls: ['./yearly-client-report.component.css'],
-  providers:[DatePipe]
+  providers: [DatePipe]
 })
 export class YearlyClientReportComponent implements OnInit {
 
 
+  data=[]
+  flag = 2;
   message
   toast
   filterForm = this.fb.group({
     startDate: [this.datePipe.transform(new Date(), 'yyyy-01-01')],
-    endDate:[this.datePipe.transform(new Date(), 'yyyy-12-31')]
+    endDate: [this.datePipe.transform(new Date(), 'yyyy-12-31')]
 
   });
 
-  constructor(private empreportService: EmpreportService, private fb: FormBuilder, private loginService: LoginService, private datePipe: DatePipe,private route: ActivatedRoute,private router: Router) { }
+  constructor(private empreportService: EmpreportService, private fb: FormBuilder, private loginService: LoginService, private datePipe: DatePipe, private route: ActivatedRoute, private router: Router) { }
 
 
 
   ngOnInit(): void {
     this.loginService.checkSessionStorage();
     this.loginService.navigateByRole(this.constructor.name)
+    this.onSubmit()
 
   }
 
-  get startDate(){
+
+
+  get startDate() {
     return this.filterForm.get("startDate")
   }
 
-  get endDate(){
+  get endDate() {
     return this.filterForm.get("endDate")
   }
 
-onSubmit(){
-  let startDate = new Date(this.startDate.value).getTime();
-  let endDate = new Date(this.endDate.value).getTime();
+  onSubmit() {
+    this.flag = 2;
+    let startDate = new Date(this.startDate.value).getTime();
+    let endDate = new Date(this.endDate.value).getTime();
 
-  if(startDate>endDate){
-    this.showToastMessage("start date cannot be after end date")
-    return
+    if (startDate > endDate) {
+      this.showToastMessage("start date cannot be after end date")
+      return
+    }
+    console.log(this.filterForm.value)
+
+
+    this.empreportService.getYearlyClientReport(this.filterForm.value).subscribe((res) => {
+      console.log(res)
+      this.data=JSON.parse(res)
+      if(this.data.length==0){
+        this.flag = 0;
+      }
+      else{
+        this.flag=1
+      }
+
+    }, (err) => {
+      console.log(err.message)
+    })
   }
-console.log(this.filterForm.value)
 
 
-this.empreportService.getYearlyClientReport(this.filterForm.value).subscribe((res)=>{
-  console.log(res)
-},(err)=>{
-  console.log(err.message)
-})
-}
-
-
-showToastMessage(message) {
-  this.message = message;
-  this.toast = true;
-  setTimeout(() => {
-    this.toast = false;
-  }, 2000)
-}
+  showToastMessage(message) {
+    this.message = message;
+    this.toast = true;
+    setTimeout(() => {
+      this.toast = false;
+    }, 2000)
+  }
 }
