bdb225c7f39502cc405b3c49c837f8230af0ccca
Simon Anil George <Simon@Anils-MBP.domain.name>
2021-09-01 16:26:03 +0530

    changed bg for landing page

--- src/app/pipes/multifilter.pipe.ts
+++ src/app/pipes/multifilter.pipe.ts
@@ -7,6 +7,9 @@ name: 'multifilter'
 export class MultifilterPipe implements PipeTransform {
 
 transform(value: any, args:any): any {
+
+
+  
   let newDate
   if (args.date != ""){
   var datePipe = new DatePipe("en-US");
@@ -18,10 +21,12 @@ transform(value: any, args:any): any {
   let data = value
   let filteredData = []
 
+ 
   filteredData = data.filter((data) => {
   return data.order_number.toLowerCase().startsWith(args.orderNumber.toString().toLowerCase()) && data.date.includes(newDate) && data.status.includes(args.status)
   && data.Client.includes(args.Client) && data.Task.includes(args.Task) && data.Process.includes(args.Process)
   })
+  // console.log(filteredData)
   return filteredData
   }
 
