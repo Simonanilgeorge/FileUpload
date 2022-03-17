42b9733a2bded8a87ac72f85daea0e87b8080ffb
Simon Anil George <Simon@Anils-MBP.domain.name>
2022-03-17 11:44:44 +0530

    dropdown component styles

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -7,10 +7,45 @@ import { Validators, FormBuilder, FormArray, FormGroup } from '@angular/forms';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
-
-  constructor() { }
+  @Input() array
+  @Input() localArray
+  @Input() display
+  @Output() displayChange=new EventEmitter()
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+    // dropdown checkbox
+    test(element, event) {
+
+      event.stopPropagation()
+  
+      if (this.array.getRawValue().includes(element)) {
+        this.array.removeAt(this.array.getRawValue().findIndex((f) => f == element))
+      }
+      else {
+        this.array.push(this.fb.control(element))
+      }
+    }
+  
+    checkAll(event) {
+  
+      event.stopPropagation()
+      this.array.clear()
+      if (event.target.checked) {
+        this.localArray.forEach((element) => {
+          this.array.push(this.fb.control(element))
+        })
+      }
+    }
+  
+    displayDropDown(event) {
+  
+      event.stopPropagation()
+      this.display = !this.display
+      this.displayChange.emit(this.display)
+  
+    }
 
 }
