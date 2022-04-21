a7584314a13429f98acc03a0f79249d8c5ba8f91
Simonanilgeorge <smnanilgeorge@gmail.com>
2022-04-21 11:13:19 +0530

    loading for monthly report

--- src/app/components/monthly-report/monthly-report.component.ts
+++ src/app/components/monthly-report/monthly-report.component.ts
@@ -4,20 +4,20 @@ import { Validators, FormBuilder, FormArray, FormGroup } from '@angular/forms';
 import { EmpreportService } from '../../providers/empreport.service'
 import { DatePipe } from '@angular/common';
 import { ExportExcelService } from '../../providers/export-excel.service'
-import {ColumnsortPipe} from '../../pipes/columnsort.pipe'
+import { ColumnsortPipe } from '../../pipes/columnsort.pipe'
 import * as XLSX from 'xlsx';
 import * as moment from 'moment'
 @Component({
   selector: 'app-monthly-report',
   templateUrl: './monthly-report.component.html',
   styleUrls: ['./monthly-report.component.css'],
-  providers:[DatePipe,ColumnsortPipe]
+  providers: [DatePipe, ColumnsortPipe]
 })
 export class MonthlyReportComponent implements OnInit {
   role
   searchedItems
-  fileName="Monthly_Production_Report.xlsx"
-  flag: Boolean = false;
+  fileName = "Monthly_Production_Report.xlsx"
+  flag = 2;
   titleName;
   message;
   toastStatus
@@ -27,11 +27,11 @@ export class MonthlyReportComponent implements OnInit {
   searchedKeyword: string;
   data = [];
   dates = [];
-  ClientList=[]
-  Tasklist=[]
-  Processlist=[]
+  ClientList = []
+  Tasklist = []
+  Processlist = []
   final
-  dropDownFilters=["client","search","task","process"];
+  dropDownFilters = ["client", "search", "task", "process"];
   dropDownList: any;
   headings = {
     "empcode": "Employee code",
@@ -40,41 +40,41 @@ export class MonthlyReportComponent implements OnInit {
     "search": "Search/Non-Search",
     "client": "Client",
     "task": "Task",
-    "process":"Process",
-    "state":"State"
+    "process": "Process",
+    "state": "State"
   }
-  titles=["empcode","name","doj","search","client","task","process","state"];
+  titles = ["empcode", "name", "doj", "search", "client", "task", "process", "state"];
   sheetNameRes;
   SheetList = ["Productivity", "Utilization", "Orders"];
   showColumnInput;
-  columnFilterForm:FormGroup=this.fb.group({
-    empcode:[""],
+  columnFilterForm: FormGroup = this.fb.group({
+    empcode: [""],
     name: [""],
     doj: [""],
     search: [""],
     client: [""],
     task: [""],
-    process:[""],
-    state:[""]
+    process: [""],
+    state: [""]
   })
   Date = this.fb.group({
-    date: [this.datePipe.transform(new Date(),"yyyy-MM"), Validators.required],
+    date: [this.datePipe.transform(new Date(), "yyyy-MM"), Validators.required],
     sheetName: ['Productivity', Validators.required]
   })
-  constructor(private loginService: LoginService, private fb: FormBuilder, private empReportService: EmpreportService,private datePipe:DatePipe,private exportExcelService: ExportExcelService,private columnSortPipe:ColumnsortPipe) { }
+  constructor(private loginService: LoginService, private fb: FormBuilder, private empReportService: EmpreportService, private datePipe: DatePipe, private exportExcelService: ExportExcelService, private columnSortPipe: ColumnsortPipe) { }
   ngOnInit(): void {
     this.loginService.checkSessionStorage();
-    this.role=sessionStorage.getItem("role").split(",")
+    this.role = sessionStorage.getItem("role").split(",")
     this.loginService.navigateByRole("MonthlyReportComponent")
     this.getDropDown();
   }
-  get task(){
+  get task() {
     return this.columnFilterForm.get("task")
   }
-  get process(){
+  get process() {
     return this.columnFilterForm.get("process")
   }
-  get client(){
+  get client() {
     return this.columnFilterForm.get("client")
   }
   get date() {
@@ -89,10 +89,10 @@ export class MonthlyReportComponent implements OnInit {
   // function called when task value is changed
   changeTaskOptions() {
     this.process.setValue("");
-    if(this.task.value==""){
-      this.Processlist=[]
+    if (this.task.value == "") {
+      this.Processlist = []
     }
-    else{
+    else {
       this.final = null
 
       this.final = this.client.value + this.task.value
@@ -103,110 +103,120 @@ export class MonthlyReportComponent implements OnInit {
   filter() {
     if (this.Date.status === "INVALID") {
       // this.showToastMessage("Select month and sheet","warning");
-      this.total=0;
+      this.flag = 0;
       return;
     }
+    this.flag = 2
     this.empReportService.getMonthlyReport(this.Date.value).subscribe((res) => {
       res = JSON.parse(res);
       this.data = res.data;
       this.dates = res.dates;
 
-      this.sheetNameRes=res.sheet;
+      this.sheetNameRes = res.sheet;
 
       this.total = 0
+
       // initialize columnsum keys to 0
-      this.dates.forEach((date)=>{
-        this.columnSum[date]=0;
+      this.dates.forEach((date) => {
+        this.columnSum[date] = 0;
       })
 
       // check if value exist in data
       this.data.forEach(datas => {
         this.total = this.total + datas.total
-        this.dates.forEach((date)=>{
-          if(datas[date]){
-            this.columnSum[date]=this.columnSum[date]+datas[date]
+        this.dates.forEach((date) => {
+          if (datas[date]) {
+            this.columnSum[date] = this.columnSum[date] + datas[date]
           }
         })
 
+
+        if (this.total == 0 && this.data.length != 0) {
+
+          this.flag = 0;
+        }
+        else {
+          this.flag = 1;
+        }
+
       });
-      this.flag = true
+
     }, (err) => {
       console.log(err.message)
     })
   }
-  showToastMessage(message,status) {
+  showToastMessage(message, status) {
     this.message = message;
-    this.toastStatus=`${status}`
+    this.toastStatus = `${status}`
     this.toast = true;
     setTimeout(() => {
       this.toast = false;
     }, 2000)
   }
-  getTitleName(title){
-    this.titleName=null;
-    setTimeout(()=>{
-      this.titleName=title;
-    },100)
+  getTitleName(title) {
+    this.titleName = null;
+    setTimeout(() => {
+      this.titleName = title;
+    }, 100)
   }
   // showInput(){
   //   this.showColumnInput = !this.showColumnInput
   // }
-  checkDay(date){
-    if(new Date(date).getDay() == 0 || new Date(date).getDay() == 6)
-    {
+  checkDay(date) {
+    if (new Date(date).getDay() == 0 || new Date(date).getDay() == 6) {
       return true
     }
-    else{
+    else {
       return false
     }
   }
   titleCase(str) {
     return str.toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
   }
-      // export to excel file
-      export() {
-        // / table id is passed over here /
-        let element = document.querySelector(".table-excel");
+  // export to excel file
+  export() {
+    // / table id is passed over here /
+    let element = document.querySelector(".table-excel");
 
-        var Heading = [];
-        this.titles.forEach(element => {
-          Heading.push(this.titleCase(this.headings[element]))
-        });
-        let i = 1;
-        this.dates.forEach(date => {
+    var Heading = [];
+    this.titles.forEach(element => {
+      Heading.push(this.titleCase(this.headings[element]))
+    });
+    let i = 1;
+    this.dates.forEach(date => {
 
-          Heading.push(`${moment(date).format("MMM")} ${i}`)
-          i+=1;
-        });
-        if(this.sheetNameRes=='Revenue'||this.sheetNameRes=='Orders'){
-          Heading.push("Total")
-        }
+      Heading.push(`${moment(date).format("MMM")} ${i}`)
+      i += 1;
+    });
+    if (this.sheetNameRes == 'Revenue' || this.sheetNameRes == 'Orders') {
+      Heading.push("Total")
+    }
 
-        const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element,{dateNF:'mm/dd/yyyy;@',cellDates:true, raw: true});
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element, { dateNF: 'mm/dd/yyyy;@', cellDates: true, raw: true });
 
-        // / generate workbook and add the worksheet /
-        const wb: XLSX.WorkBook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    // / generate workbook and add the worksheet /
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
-        XLSX.utils.sheet_add_aoa(ws, [Heading], {origin:"A2"});
+    XLSX.utils.sheet_add_aoa(ws, [Heading], { origin: "A2" });
 
-        ws['!rows'][0] = { hidden: true };
-        // / save to file /
-        XLSX.writeFile(wb, this.fileName);
+    ws['!rows'][0] = { hidden: true };
+    // / save to file /
+    XLSX.writeFile(wb, this.fileName);
 
-      }
-   public searchItems() {
-     this.searchedItems = this.columnSortPipe.transform(this.data,this.columnFilterForm.value);
+  }
+  public searchItems() {
+    this.searchedItems = this.columnSortPipe.transform(this.data, this.columnFilterForm.value);
     return this.searchedItems;
-}
-getDropDown() {
-  this.empReportService.getDropDownList().subscribe((res) => {
-    this.dropDownList = res;
-    this.ClientList = this.dropDownList.Client;
+  }
+  getDropDown() {
+    this.empReportService.getDropDownList().subscribe((res) => {
+      this.dropDownList = res;
+      this.ClientList = this.dropDownList.Client;
 
-    this.filter()
-  }, (err) => {
-    console.log(err.message)
-  })
-}
+      this.filter()
+    }, (err) => {
+      console.log(err.message)
+    })
+  }
 }
